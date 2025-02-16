9c9cc4259f5d5a84e2e72ad21e7e8fdb8f485669
Partha <parthapratimpaul2003@gmail.com>
2025-02-16 19:17:10 +0530

    Added Username Field to User

--- server/mongodb/models/user.js
+++ server/mongodb/models/user.js
@@ -1,55 +1,59 @@
 // models/user.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    userId: {
-        type: String, // Consistent with the `userId` used in other schemas
-        required: true,
-        unique: true,
+  userId: {
+    type: String, // Consistent with the `userId` used in other schemas
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+  },
+  username: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+  },
+  imageGenerationCount: {
+    type: Number,
+    default: 0,
+  },
+  postCount: {
+    type: Number,
+    default: 0,
+  },
+  likeCount: {
+    type: Number,
+    default: 0, // Total likes on all posts by this user
+  },
+  commentCount: {
+    type: Number,
+    default: 0, // Total comments by this user
+  },
+  followers: [
+    {
+      type: String, // Array of `userId`s following this user
     },
-    name: {
-        type: String,
+  ],
+  following: [
+    {
+      type: String, // Array of `userId`s this user is following
     },
-    email: {
-        type: String,
+  ],
+  bookmarks: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
     },
-    imageGenerationCount: {
-        type: Number,
-        default: 0,
-    },
-    postCount: {
-        type: Number,
-        default: 0,
-    },
-    likeCount: {
-        type: Number,
-        default: 0, // Total likes on all posts by this user
-    },
-    commentCount: {
-        type: Number,
-        default: 0, // Total comments by this user
-    },
-    followers: [
-        {
-            type: String, // Array of `userId`s following this user
-        },
-    ],
-    following: [
-        {
-            type: String, // Array of `userId`s this user is following
-        },
-    ],
-    bookmarks: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Post'
-        }
-    ],
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
